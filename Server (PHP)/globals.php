08052011eb7fd3bb8e1a0aab7cb73030214cf890
Benjamin <biitez@obito.dev>
2021-04-28 21:51:58 +0200

    Update globals.php

--- Server (PHP)/globals.php
+++ Server (PHP)/globals.php
@@ -8,3 +8,5 @@ define("DB_DATABASE", "database_name" );
 define("URL_PAGE", "https://example.com/server/panel.php"); // Here you write the link where the panel.php file is, you need to put it with HTTPS, for example, https://example.com/panel.php
 define("API_KEY_COINBASE_COMMERCE", "your coinbase api key");
 define("SECRET_KEY_WEBHOOK_COINBASE_COMMERCE", "your secret key webhook coinbase commerce");
+
+?>
