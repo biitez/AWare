d6d7305bfb0b907df1003e121a5e1bf7a3a43832
biitez <biitez@outlook.es>
2022-03-29 01:09:56 -0300

    renamed PHP file

--- Server PHP Files/inc/CryptAPI.php
+++ Server PHP Files/inc/CryptAPI.php
@@ -4,17 +4,20 @@ class CryptAPI
 {
     private $BaseUrl = 'https://api.cryptapi.io';
 
-    public function GenerateAddress($Callback) 
+    public function GenerateAddress($Callback, $BTCAddress) 
     {
         try
         {
-            $httpStringResponse = @file_get_contents($this->BaseUrl . '/btc/create?' . http_build_query([
+            $CryptApiUrl = $this->BaseUrl . '/btc/create?' . http_build_query([
                 'callback' => $Callback,
-                'address' => 'bc1qm8f04zx0npmskp3wqgxpr8dfv0d5yer5kvnl4g',
-                'pending' => '0',
+                'address' => $BTCAddress,
                 'confirmations' => '1',
                 'priority' => 'fast'
-            ]));
+            ]);
+
+            $httpStringResponse = @file_get_contents($CryptApiUrl);
+
+            die($httpStringResponse);
 
             preg_match('/([0-9])\d+/',$http_response_header[0],$matches);
 
